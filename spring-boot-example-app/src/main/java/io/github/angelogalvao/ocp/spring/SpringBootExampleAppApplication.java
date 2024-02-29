a2beda97484cd18b2467955f9ff73844ee1488d1
Ângelo Galvão <angelogalvao@gmail.com>
2024-02-29 00:07:40 -0500

    Spring Boot application to test on Openshift

---
+++ spring-boot-example-app/src/main/java/io/github/angelogalvao/ocp/spring/SpringBootExampleAppApplication.java
@@ -0,0 +1,16 @@
+package io.github.angelogalvao.ocp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.dekorate.openshift.annotation.OpenshiftApplication;
+
+@SpringBootApplication
+@OpenshiftApplication(replicas = 3, expose = true)
+public class SpringBootExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleAppApplication.class, args);
+	}
+
+}

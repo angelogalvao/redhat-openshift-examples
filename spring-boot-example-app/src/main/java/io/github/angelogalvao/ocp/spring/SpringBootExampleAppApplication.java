bc34f572387739e2608be2afee5f936927aeed73
Ângelo Galvão <angelogalvao@gmail.com>
2024-02-29 11:18:39 -0500

    Fixing Dekorate version!

--- spring-boot-example-app/src/main/java/io/github/angelogalvao/ocp/spring/SpringBootExampleAppApplication.java
+++ spring-boot-example-app/src/main/java/io/github/angelogalvao/ocp/spring/SpringBootExampleAppApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.dekorate.openshift.annotation.OpenshiftApplication;
+import io.dekorate.openshift.annotation.Route;
 
 @SpringBootApplication
-@OpenshiftApplication(replicas = 3, expose = true)
+@OpenshiftApplication(replicas = 3, route = @Route(expose = true)) 
 public class SpringBootExampleAppApplication {
 
 	public static void main(String[] args) {

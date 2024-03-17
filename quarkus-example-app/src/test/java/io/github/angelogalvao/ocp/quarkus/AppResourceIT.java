b7ddb8ec559688d8cb08580650e2477a1eb42aa3
Ângelo Galvão <angelogalvao@gmail.com>
2024-03-16 20:51:23 -0400

    Quarkus Example App commit

---
+++ quarkus-example-app/src/test/java/io/github/angelogalvao/ocp/quarkus/AppResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.angelogalvao.ocp.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AppResourceIT extends AppResourceTest {
+    // Execute the same tests but in packaged mode.
+}

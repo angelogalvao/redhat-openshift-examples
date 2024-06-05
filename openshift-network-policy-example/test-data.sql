d973a3e76579dec6c980d1a66064339193a783e3
Ângelo Galvão <angelogalvao@gmail.com>
2024-06-04 21:12:25 -0400

    Openshift Network Policy Example

---
+++ openshift-network-policy-example/test-data.sql
@@ -0,0 +1,8 @@
+create table test (
+    test_id int primary key,
+    test_value varchar(10)
+);
+
+insert into test values (1, "TEST 1");
+insert into test values (2, "TEST 2");
+insert into test values (3, "TEST 3");
